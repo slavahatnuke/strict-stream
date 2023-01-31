e3f66a30b841ee4b460287f3b593651cb2fb9afd
slava <slava.hatnuke@gmail.com>
2023-01-31 03:01:33 +0000

    tap, map, filter

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,98 @@ export type TypedStream<Type> = AsyncIterable<Type>;
 export type TypedMapper<Input, Output> = (stream: TypedStream<Input>) => TypedStream<Output>;
 
 export type TypedPipe<Input> = TypedStream<Input> & {
-    and<Output>(mapper: TypedMapper<Input, Output>): TypedPipe<Output>
+    Then<Output>(mapper: TypedMapper<Input, Output>): TypedPipe<Output>
 };
 
-export function pipe<Input>(stream: TypedStream<Input>): TypedPipe<Input> {
+const ok = (ok: any, error: string) => {
+    if (!ok) {
+        throw new Error(error)
+    }
+}
+
+type TypedIterator<Type> = AsyncIterator<Type>;
+
+function streamToIterator<Type>(stream: TypedStream<Type>): TypedIterator<Type> {
+    return stream[Symbol.asyncIterator]();
+}
+
+function iteratorFactoryToStream<Type>(iteratorFactory: () => TypedIterator<Type>): TypedStream<Type> {
     return {
-        ...stream,
-        and<Output>(mapper: TypedMapper<Input, Output>) {
-            return pipe(mapper(stream))
-        }
+        [Symbol.asyncIterator]: iteratorFactory,
+    };
+}
+
+export function Pipe<Input>(stream: TypedStream<Input>): TypedPipe<Input> {
+    ok(stream[Symbol.asyncIterator], 'No async iterator in stream')
+
+    return {
+        [Symbol.asyncIterator]: () => streamToIterator(stream),
+        Then: <Output>(mapper: TypedMapper<Input, Output>) => {
+            return Pipe(iteratorFactoryToStream(() => streamToIterator(mapper(stream))))
+        },
     }
 }
 
-async function* foo(): TypedStream<string> {
-    yield await '1';
-    yield await '2';
+async function* foo(): TypedStream<number> {
+    yield await 1;
+    yield await 2;
 }
 
-pipe(foo())
-    .and((stream) => stream)
 
-async function run<T extends any>(stream: TypedStream<T>) {
+async function run<T extends any>(stream: TypedStream<T>): Promise<T | undefined> {
+    let value: T | undefined = undefined;
     for await (const record of stream) {
-        console.log(record)
+        value = record
     }
+    return value
+}
+
+type MaybeAsync<Type> = Type | Promise<Type>;
+
+function map<Input, Output>(mapper: (input: Input) => MaybeAsync<Output>): TypedMapper<Input, Output> {
+    return (inputStream) => {
+        async function* mappedStream(): TypedStream<Output> {
+            for await (const record of inputStream) {
+                yield await mapper(record)
+            }
+        }
+
+        return mappedStream();
+    };
+}
+
+function tap<Input>(tapper: (input: Input) => MaybeAsync<any>) {
+    return map<Input, Input>(async (input) => {
+        await tapper(input)
+        return input
+    })
+}
+
+
+function filter<Input>(condition: (input: Input) => MaybeAsync<boolean | undefined | null>): TypedMapper<Input, Input> {
+    return (input) => {
+        async function* filtered(): TypedStream<Input> {
+            for await (const record of input) {
+                const ok = await condition(record);
+                if (ok) {
+                    yield record
+                }
+            }
+        }
+
+        return filtered();
+    };
 }
 
 async function app() {
-    const stream = foo();
-    await run(stream)
-    await run(stream)
+    const x = Pipe(foo())
+        .Then(tap(console.log))
+        .Then(filter((x) => x > 1))
+        .Then(map((a): { name: string } => ({name: String(a)})))
+        .Then(map((x) => ({...x, ok: true})))
+        .Then(tap(console.log));
+
+    await run(x)
 }
 
 app().catch(console.error)
