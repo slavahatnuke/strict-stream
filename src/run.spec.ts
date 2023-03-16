8cc32b084a22a0dc40849f3d403097ce3a299b88
slava <slava.hatnuke@gmail.com>
2023-03-16 04:02:25 +0000

    ...

---
+++ src/run.spec.ts
@@ -0,0 +1,25 @@
+import {describe, expect, it} from "vitest";
+import {from} from "./from";
+import {toArray} from "./toArray";
+import {reduce} from "./reduce";
+import {run} from "./index";
+
+describe(run.name, () => {
+    it('returns last value', async () => {
+        const stream = from([
+            1, 2, 3
+        ])
+        const lastValue = await run(stream, null);
+        expect(lastValue).toEqual(3)
+    });
+
+    it('returns default value when stream is empty', async () => {
+        const stream = from([
+
+        ])
+        const defaultValue = Symbol('TO_TEST');
+        const lastValue = await run(stream, defaultValue);
+        expect(lastValue).toEqual(defaultValue)
+    });
+
+})
