74148c46af7c0cee6921a5968f341508004cef3d
slava <slava.hatnuke@gmail.com>
2023-03-16 03:49:59 +0000

    ...

---
+++ src/batch.spec.ts
@@ -0,0 +1,20 @@
+import {describe, expect, it} from "vitest";
+import {from} from "./from";
+import {toArray} from "./toArray";
+import {batch} from "./batch";
+
+describe(batch.name, () => {
+    it('filter values', async () => {
+        const stream = from([
+            1, 2, 3
+        ])
+
+            .pipe(
+                batch(2)
+            )
+
+        const outputs = await toArray(stream);
+
+        expect(outputs).toEqual([[1, 2], [3]])
+    });
+})
