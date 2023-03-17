2d1bd7d7db2e8f5c25333812619ec8d7d52cfb44
slava <slava.hatnuke@gmail.com>
2023-03-17 04:18:09 +0000

    ...

--- src/vitest.post-release.config.ts
+++ src/vitest.post-release.config.ts
@@ -2,6 +2,6 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        exclude: [...configDefaults.exclude, 'src/**.spec.ts'],
+        exclude: [...configDefaults.exclude, 'src/**.spec.ts', 'src/**/*.spec.ts'],
     },
 })
