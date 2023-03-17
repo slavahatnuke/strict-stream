cd9d1391138fc3ccded3696e779c67d796010128
slava <slava.hatnuke@gmail.com>
2023-03-17 05:06:22 +0000

    ...

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # strict-stream
 
+## 1.0.2
+
+### Patch Changes
+
+- updated build
+
 ## 1.0.1
 
 ### Patch Changes
--- cd/post-release.spec.ts
+++ cd/post-release.spec.ts
@@ -4,6 +4,7 @@ import {map} from "strict-stream/map";
 import {toArray} from "strict-stream/toArray";
 
 import {describe, expect, it} from "vitest";
+import {scale} from "../src/scale";
 
 describe('post-release', () => {
     it('test package.json', async () => {
@@ -13,7 +14,7 @@ describe('post-release', () => {
         expect(installedVersion.version).toEqual(currentVersion.version)
     });
 
-    it('smoke test', async () => {
+    it('smoke test / map', async () => {
         const stream = from(sequence(3))
             .pipe(map((value) => value + 100))
 
@@ -21,6 +22,19 @@ describe('post-release', () => {
         const outputs = await toArray(stream);
 
         expect(outputs).toEqual([
+            100,
+            101,
+            102,
+        ])
+    });
+    it('smoke test / scale', async () => {
+        const stream = from(sequence(3))
+            .pipe(scale(1, (value) => value + 100))
+
+
+        const outputs = await toArray(stream);
+
+        expect(outputs.sort()).toEqual([
             100,
             101,
             102,
